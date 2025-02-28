663ecce5758b12d01a5629d36f6ba13ea123ef99
lizidelyx <alizza.deli28@gmail.com>
2025-02-28 20:33:51 +0700

    add read.md

--- player.gd
+++ player.gd
@@ -1,42 +1,51 @@
 extends CharacterBody2D
+
 @export var walk_speed = 200
+@export var crouch_speed = 100
 @export var gravity = 200.0
 @export var jump_speed = -300
-@onready var sprite = $AnimatedSprite2D  # Pastikan node AnimatedSprite2D ada sebagai child dari CharacterBody2D
+@export var push_force = 500.0  # Kekuatan dorongan terhadap box
+@onready var sprite = $AnimatedSprite2D
+var push = false
+var jumps_left = 2
+var is_crouching = false
 
-var jumps_left = 2  # Menyimpan jumlah lompatan yang tersisa
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -walk_speed
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = walk_speed
-	else:
-		velocity.x = 0
-	move_and_slide()
 
 func _physics_process(delta):
 	velocity.y += delta * gravity
 
 	if is_on_floor():
-		jumps_left = 2  # Reset jumlah lompatan saat menyentuh tanah
+		jumps_left = 2
+		
+	
 
 	if Input.is_action_just_pressed("ui_up") and jumps_left > 0:
 		velocity.y = jump_speed
-		jumps_left -= 1  # Mengurangi jumlah lompatan yang tersisa
+		jumps_left -= 1
+
+	if Input.is_action_pressed("ui_down"):
+		is_crouching = true
+		sprite.play("crouch")
+	else:
+		is_crouching = false
+
+	var move_direction = 0
 
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -walk_speed
-		sprite.flip_h = true  # Balik sprite jika berjalan ke kiri
-		sprite.play("walk")
+		move_direction = -1
+		sprite.flip_h = true
+		if not is_crouching:
+			sprite.play("walk")
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = walk_speed
-		sprite.flip_h = false  # Kembali ke posisi normal jika berjalan ke kanan
-		sprite.play("walk")
+		move_direction = 1
+		sprite.flip_h = false
+		if not is_crouching:
+			sprite.play("walk")
 	else:
-		velocity.x = 0
-		sprite.play("idle")  # Jika tidak bergerak, mainkan animasi idle
+		if not is_crouching:
+			sprite.play("idle")
+
+	velocity.x = move_direction * (crouch_speed if is_crouching else walk_speed)
+
 
 	move_and_slide()
